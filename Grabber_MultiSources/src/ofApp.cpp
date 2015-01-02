37a0502b14553cfbbfee31b6bfc906d96e373904
vj.dudley.smith@gmail.com <vj.dudley.smith@gmail.com>
2015-01-02 17:49:49 +0100

    Adding kinect support (new project)

---
+++ Grabber_MultiSources/src/ofApp.cpp
@@ -0,0 +1,407 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+    
+    ofSetLogLevel(OF_LOG_VERBOSE);
+    
+    // Setup panels
+    setupPanelOpenCv();
+    setupPanelSource();
+    
+    switch (m_pSource) {
+        case 0:
+            // Ip camera -----------
+            setupIpCamera();
+            setupPanelGrabber();
+            break;
+        case 1:
+            // Movei file
+            break;
+        case 2:
+            // Kinect -----
+            setupKinect();
+            break;
+            
+        default:
+            break;
+    }
+
+    setupOpenCv();
+    setupOsc();
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::setupPanelGrabber(){
+    bluestrokGrabber = ofPtr<ofxBluestorkGrabber>(new ofxBluestorkGrabber);
+    bluestrokGrabber->setCameraAddress(m_pAdress);
+    
+    grabberPanel.setup(bluestrokGrabber->parameters);
+    grabberPanel.setPosition(20, 550);
+}
+
+//--------------------------------------------------------------
+void ofApp::setupIpCamera(){
+    videoGrabber.setGrabber(bluestrokGrabber);
+    videoGrabber.initGrabber(m_pGrabWidth,m_pGrabHeight);
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::setupPanelSource(){
+    cameraPanel.setup("grabber", "grabber.xml");
+    
+    cameraPanel.add(m_pSource.set("source", 0, 0, 2));
+    cameraPanel.add(m_pSourceDisplay.set("sourceDisplay", ""));
+    cameraPanel.add(m_pAdress.set("adress", "192.168.3.101"));
+    cameraPanel.add(m_pGrabWidth.set("grabWidth", 320, 320, 1280));
+    cameraPanel.add(m_pGrabHeight.set("grabHeight", 240, 240, 768));
+    cameraPanel.add(m_pDrawWidth.set("drawWidth", 320, 320, 1280));
+    cameraPanel.add(m_pDrawHeight.set("drawHeight", 240, 240, 768));
+    
+    cameraPanel.setPosition(260, 550);
+    cameraPanel.loadFromFile("grabber.xml");
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::setupPanelOpenCv(){
+    // Panel setup
+    m_pSetBg.addListener(this,&ofApp::setBgPressed);
+    
+    openCvPanel.setup("openCv", "openCv.xml");
+    openCvPanel.add(m_pTwoThreshold.set("twoThreshold", true));
+    openCvPanel.add(m_pThreshold.set("threshold", 80, 0, 255));
+    openCvPanel.add(m_pNearThreshold.set("nearThreshold", 80, 0, 255));
+    openCvPanel.add(m_pFarThreshold.set("farRhreshold", 80, 0, 255));
+    openCvPanel.add(m_pBrightness.set("brightness", 0, -1, 1));
+    openCvPanel.add(m_pContrast.set("contrast", 0, -1, 1));
+    openCvPanel.add(m_pBlur.set("blur", false));
+    openCvPanel.add(m_pSetBg.setup("Set Background"));
+    openCvPanel.add(m_pDraw.setup("Draw", true));
+    openCvPanel.loadFromFile("openCv.xml");
+    openCvPanel.setPosition(500, 550);
+}
+
+//--------------------------------------------------------------
+void ofApp::setupOpenCv(){
+    colorImg.allocate(m_pGrabWidth,m_pGrabHeight);
+    grayImage.allocate(m_pGrabWidth,m_pGrabHeight);
+    grayThreshNear.allocate(m_pGrabWidth,m_pGrabHeight);
+    grayThreshFar.allocate(m_pGrabWidth,m_pGrabHeight);
+    grayBg.allocate(m_pGrabWidth,m_pGrabHeight);
+    grayDiff.allocate(m_pGrabWidth,m_pGrabHeight);
+    //grayToCompute.allocate(320, 240);
+}
+
+
+//--------------------------------------------------------------
+void ofApp::setupOsc(){
+    
+    oscPanel.setup("osc", "osc.xml");
+    oscPanel.add(m_pReady.set("ready", false));
+    oscPanel.add(m_pSend.set("send", true));
+    oscPanel.add(m_pHost.set("host", "127.0.0.1"));
+    oscPanel.add(m_pPort.set("port", 9001));
+    oscPanel.add(m_pPrefix.set("prefix", "cam1"));
+    
+    oscPanel.setPosition(750, 550);
+    oscPanel.loadFromFile("osc.xml");
+    
+    oscSender.setup(m_pHost, m_pPort);
+
+}
+
+void ofApp::setupKinect(){
+    // enable depth->video image calibration
+    kinect.setRegistration(true);
+    
+    kinect.init();
+    //kinect.init(true); // shows infrared instead of RGB video image
+    //kinect.init(false, false); // disable video image (faster fps)
+    
+    kinect.open();		// opens first available kinect
+    //kinect.open(1);	// open a kinect by id, starting with 0 (sorted by serial # lexicographically))
+    //kinect.open("A00362A08602047A");	// open a kinect using it's unique serial #
+    
+    // print the intrinsic IR sensor values
+    if(kinect.isConnected()) {
+        ofLogNotice() << "sensor-emitter dist: " << kinect.getSensorEmitterDistance() << "cm";
+        ofLogNotice() << "sensor-camera dist:  " << kinect.getSensorCameraDistance() << "cm";
+        ofLogNotice() << "zero plane pixel size: " << kinect.getZeroPlanePixelSize() << "mm";
+        ofLogNotice() << "zero plane dist: " << kinect.getZeroPlaneDistance() << "mm";
+    }
+    
+    m_pGrabWidth = (int)kinect.width;
+    m_pGrabHeight = (int)kinect.height;
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+    
+    bool doUpdate = false;
+    
+    switch (m_pSource) {
+        case 0:
+            // Ip camera -----------
+            updateIpCam();
+            doUpdate = videoGrabber.isFrameNew();
+            if (doUpdate==true) {
+                colorImg.setFromPixels(videoGrabber.getPixels(), m_pGrabWidth, m_pGrabHeight);
+                grayImage = colorImg;
+            }
+            break;
+        case 1:
+            // Movei file
+            break;
+        case 2:
+            // Kinect -----
+            updateKinect();
+            doUpdate = kinect.isFrameNew();
+            if (doUpdate==true) {
+                grayImage.setFromPixels(kinect.getDepthPixels(), m_pGrabWidth, m_pGrabHeight);
+            }
+            break;
+            
+        default:
+            break;
+    }
+    
+    if (doUpdate==true) {
+        updateOpenCv();
+        updateOsc();
+    }
+}
+
+//--------------------------------------------------------------
+void ofApp::updateIpCam(){
+    videoGrabber.update();
+}
+
+//--------------------------------------------------------------
+void ofApp::updateKinect(){
+    kinect.update();
+}
+
+
+//--------------------------------------------------------------
+void ofApp::updateOpenCv(){
+    
+    grayImage.brightnessContrast(m_pBrightness, m_pContrast);
+    
+    // take the abs value of the difference between background and incoming and then threshold:
+    grayDiff.absDiff(grayBg, grayImage);
+    
+    if(m_pTwoThreshold == true){
+        // Old way : one threshold
+        grayDiff.threshold(m_pThreshold);
+    }else{
+        // New way
+        grayThreshNear = grayImage;
+        grayThreshFar = grayImage;
+        grayThreshNear.threshold(m_pNearThreshold, true);
+        grayThreshFar.threshold(m_pFarThreshold);
+        cvAnd(grayThreshNear.getCvImage(), grayThreshFar.getCvImage(), grayImage.getCvImage(), NULL);
+    }
+
+    if(m_pBlur)
+        grayDiff.blurHeavily();
+    
+    // find contours which are between the size of 20 pixels and 1/3 the w*h pixels.
+    // also, find holes is set to true so we will get interior contours as well....
+    contourFinder.findContours(grayDiff, 20, (340*240)/3, 10, true);	// find holes
+    
+}
+
+
+//--------------------------------------------------------------
+void ofApp::updateOsc(){
+    
+    
+    if (reportOsc.length() > 1000) {
+        reportOsc = "";
+    }
+
+   for (int i = 0; i < contourFinder.nBlobs; i++){
+        
+        ofPoint center = contourFinder.blobs[i].boundingRect.getCenter();
+        float radius = sqrt(contourFinder.blobs[i].area/PI);
+        
+        ofxOscMessage m = getMessage(i, center, radius);
+        oscSender.sendMessage(m);
+        
+        string message = "Message Osc : " + m.getAddress() + ":" ;
+        
+        vector<ofxOscArg>::iterator arg;
+        for (int nbArgs=0; nbArgs<m.getNumArgs(); nbArgs++) {
+            message += m.getArgAsString(nbArgs) + " - ";
+        }
+        
+        reportOsc.append(message);
+        ofLogVerbose() << message;
+        
+    }
+    
+    
+}
+
+//-----------------------------------------------------------------------------
+ofxOscMessage ofApp::getMessage(int _blobIndex, ofPoint _center, float _radius){
+    
+    ofxOscMessage m;
+    string prefix = m_pPrefix;
+    
+    m.setAddress("/" + prefix + "/blob" + ofToString(_blobIndex, 3, '0'));
+    m.addFloatArg((float)(_center.x)/m_pGrabWidth);
+    m.addFloatArg((float)(_center.y)/m_pGrabHeight);
+    m.addFloatArg(_radius);
+    
+    return m;
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::setBgPressed(){
+    grayBg = grayImage;		// the = sign copys the pixels from grayImage into grayBg (operator overloading)
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+    
+    if(m_pDraw){
+        drawOpenCv();
+    }
+    
+    switch (m_pSource) {
+        case 0:
+            m_pSourceDisplay = "IP camera";
+            break;
+        case 1:
+            m_pSourceDisplay = "movie file";
+            break;
+        case 2:
+            m_pSourceDisplay = "kinect";
+            break;
+            
+        default:
+            break;
+    }
+    
+    grabberPanel.draw();
+    cameraPanel.draw();
+    openCvPanel.draw();
+    oscPanel.draw();
+    
+    // Report Blob
+    ofSetColor(ofColor::white);
+    stringstream reportStr;
+    
+    reportStr << "bg subtraction and blob detection" << endl
+    << "threshold " << m_pThreshold << endl
+    << "num blobs found " << contourFinder.nBlobs << endl
+    << "fps: " << ofGetFrameRate();
+    
+    ofDrawBitmapString(reportStr.str(), 750, 360);
+    
+    // Report OSC
+    ofDrawBitmapString(reportOsc, 750, 500);
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::drawOpenCv(){
+    
+    // draw the incoming, the grayscale, the bg and the thresholded difference
+    ofSetHexColor(0xffffff);
+    switch (m_pSource) {
+        case 0:
+            colorImg.draw(20,20,m_pDrawWidth, m_pDrawHeight);
+            break;
+        case 1:
+            // Draw movie here
+            break;
+        case 2:
+            kinect.drawDepth(20, 20, m_pDrawWidth, m_pDrawHeight);
+            break;
+            
+        default:
+            break;
+    }
+    grayImage.draw(360,20,m_pDrawWidth, m_pDrawHeight);
+    grayBg.draw(20,280,m_pDrawWidth, m_pDrawHeight);
+    grayDiff.draw(360,280,m_pDrawWidth, m_pDrawHeight);
+    //grayToCompute.draw(20, 540);
+    
+    // Draw background of blobs ---
+    
+    ofPushStyle();
+    ofPushMatrix();
+    ofTranslate(700, 20);
+    
+    ofPushStyle();
+    ofFill();
+    ofSetColor(ofColor::darkGrey);
+    ofRect(0, 0,320,240);
+    ofPopStyle();
+    
+    ofNoFill();
+    ofSetColor(ofColor::white);
+    ofSetLineWidth(2);
+    
+    for (int i = 0; i < contourFinder.nBlobs; i++){
+        ofPoint center = contourFinder.blobs[i].boundingRect.getCenter();
+        float radius = sqrt(contourFinder.blobs[i].area/PI);
+        ofCircle(center, radius);
+    }
+    ofPopMatrix();
+    ofPopStyle();
+
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+    
+}
---
+++ Grabber_MultiSources/src/ofApp.h
@@ -0,0 +1,101 @@
+#pragma once
+
+#include "ofMain.h"
+#include "ofxBluestorkGrabber.h"
+#include "ofxGui.h"
+#include "ofxOpenCv.h"
+#include "ofxOsc.h"
+#include "ofxKinect.h"
+
+class ofApp : public ofBaseApp{
+    
+public:
+    void setup();
+    
+    void setupPanelGrabber();
+    void setupPanelSource();
+    void setupPanelOpenCv();
+    
+    void setupOpenCv();
+    void setupOsc();
+    
+    void setupIpCamera();
+    void setupKinect();
+    
+    void update();
+    void updateIpCam();
+    void updateKinect();
+    
+    void updateOpenCv();
+    void updateOsc();
+    void setBgPressed();
+    
+    void draw();
+    void drawOpenCv();
+    
+    void keyPressed(int key);
+    void keyReleased(int key);
+    void mouseMoved(int x, int y );
+    void mouseDragged(int x, int y, int button);
+    void mousePressed(int x, int y, int button);
+    void mouseReleased(int x, int y, int button);
+    void windowResized(int w, int h);
+    void dragEvent(ofDragInfo dragInfo);
+    void gotMessage(ofMessage msg);
+
+    
+    // Grabber -------------------------------------
+    ofPtr<ofxBluestorkGrabber> bluestrokGrabber;
+    ofVideoGrabber videoGrabber;
+    
+    ofxPanel grabberPanel;
+    
+    ofParameter<string> m_pAdress;
+    ofParameter<int>    m_pSource;
+    ofParameter<string> m_pSourceDisplay;
+    ofParameter<int>    m_pGrabWidth;
+    ofParameter<int>    m_pGrabHeight;
+    ofParameter<int>    m_pDrawWidth;
+    ofParameter<int>    m_pDrawHeight;
+    ofxToggle           m_pDraw;
+    ofxPanel            cameraPanel;
+    
+    // OpenCv -----------------------------------------
+    ofxCvColorImage			colorImg;
+    
+    ofxCvGrayscaleImage 	grayImage;
+    ofxCvGrayscaleImage 	grayBg;
+    ofxCvGrayscaleImage 	grayDiff;
+    ofxCvGrayscaleImage     grayThreshNear; // the near thresholded image
+    ofxCvGrayscaleImage     grayThreshFar; // the far thresholded image
+    
+    ofxCvContourFinder 	contourFinder;
+    
+    ofParameter<bool>   m_pTwoThreshold;
+    ofParameter<int>    m_pThreshold;
+    ofParameter<int>    m_pNearThreshold;
+    ofParameter<int>    m_pFarThreshold;
+    ofParameter<float>  m_pBrightness;
+    ofParameter<float>  m_pContrast;
+    ofParameter<bool>   m_pBlur;
+    ofxButton           m_pSetBg;
+    ofxPanel            openCvPanel;
+    
+    // OSC -----------------------------------------
+    ofxOscSender        oscSender;
+    string              reportOsc;
+    
+    ofxPanel            oscPanel;
+    ofParameter<bool>       m_pReady;
+    ofParameter<bool>       m_pSend;
+    ofParameter<string>     m_pHost;
+    ofParameter<int>        m_pPort;
+    ofParameter<string>     m_pPrefix;
+    
+    ofxOscMessage getMessage(int _blobIndex, ofPoint _center, float _radius);
+    
+    // Kinect --------------------------------------
+    ofxKinect kinect;
+    
+};
+
